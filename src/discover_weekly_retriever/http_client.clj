0b92826d37f3c22ad38643b26f4335ed294b5d9b
Luciana Marques <lucianadacostamarques@gmail.com>
2020-03-07 22:15:58 -0300

    Experiments with http client

--- src/discover_weekly_retriever/http_client.clj
+++ src/discover_weekly_retriever/http_client.clj
@@ -1,11 +1,22 @@
 (ns discover-weekly-retriever.http-client
-  (require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.string :as str]))
 
-(defn call-client-head []
-  (client/head "https://api.spotify.com/v1"))
+(def base-url "https://api.spotify.com/v1")
+
+(def spotify-uri "spotify:user:22oaqqtlz7vh26hghdmd5wf3q")
+
+(defn get-spotify-id [spotify-uri]
+  (-> spotify-uri (str/split #":") (peek)))
+
+(defn get-user []
+  (client/request
+   {:url (str base-url "v1/users/" (get-spotify-id spotify-uri))
+   :method :get
+   :basic-auth token}))
 
 (comment
- (call-client-head)
+ (get-user)
  (print *e))
 
 
